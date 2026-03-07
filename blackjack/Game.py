92ca34bb9cb1cfb0ccc5aa5a72c58c28a7309dd6
Louie Jay Oga <kmshinichi@gmail.com>
2026-03-07 19:27:13 +0800

    Refactored Main

--- blackjack/Game.py
+++ blackjack/Game.py
@@ -1,28 +1,8 @@
-from SlowPrint import slow_print
 from GameModes import Game
 
 def main():
-    slow_print("===== Welcome to Blackjack =====\n")
-    slow_print("===== Game Modes =====")
-    slow_print("1. You vs AI")
-    slow_print("2. You vs Another Human")
-    while True:
-        try:
-            gm_choice = int(input("\nChoose a game mode: "))
-            if gm_choice == 1:
-                game = Game.OnePlayer()
-                game.play_round()
-                break
-
-            elif gm_choice == 2:
-                game = Game.TwoPlayers()
-
-            else:
-                slow_print("Out of option's range, try again.")
-
-        except ValueError:
-            slow_print("Invalid input, try again.")
+    game = Game.OnePlayer()
+    return game
 
 if __name__ == "__main__":
     main()
-#
