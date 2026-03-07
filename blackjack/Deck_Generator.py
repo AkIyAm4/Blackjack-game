7d9f519708361f0720ae14f2c4617948eedd75c7
Louie Jay Oga <kmshinichi@gmail.com>
2026-03-07 19:07:54 +0800

    Fixed Flow for GUI
    Replaced slow_print with print for user prompts and messages. Removed unused imports and cleaned up the code structure.

--- blackjack/Deck_Generator.py
+++ blackjack/Deck_Generator.py
@@ -1,34 +1,22 @@
 import random
-import time
-from SlowPrint import slow_print
-#
+
 def shuffling_choice():
-    slow_print("===== Shuffling Methods =====")
-    slow_print("1. Random Shuffle")
-    slow_print("2. Riffle Shuffle")
-    slow_print("3. Overhand Shuffle")
+    print("===== Shuffling Methods =====")
+    print("1. Random Shuffle")
+    print("2. Riffle Shuffle")
+    print("3. Overhand Shuffle")
     while True:
-        try:
-            choice = int(input("Choose a shuffling method: "))
+        choice = int(input("Choose a shuffling method: "))
 
-            deck = Shuffling()
+        deck = Shuffling()
 
-            if choice == 1:
-                deck.random()
-                return deck.cards
+        if choice == 1:
+            deck.random()
+            return deck.cards
 
-            elif choice == 2:
-                deck.riffle()
-                return deck.cards
-
-            elif choice == 3:
-                slow_print("Under Maintenance.")
-
-            else:
-                slow_print("Input is out of option's range, try again.")
-
-        except ValueError:
-            slow_print("Invalid input, try again.")
+        elif choice == 2:
+            deck.riffle()
+            return deck.cards
 
 class Shuffling:
     def __init__(self):
@@ -53,9 +41,6 @@ class Shuffling:
             self.cards[:] = shuffled
         return self.cards
 
-    def overhand(self):
-        pass
-
 class Deck:
     def __init__(self):
         self.cards = shuffling_choice()
@@ -63,18 +48,8 @@ class Deck:
     def draw(self):
         return self.cards.pop()
 
-    def deck_regen(self):
+    def deck_regen(self): # NEEDS FIXING
         if len(self.cards) < 11:
-            slow_print("\nRegenerating the deck, please wait...")
-            time.sleep(2)
+            print("\nRegenerating the deck, please wait...")
             self.__init__()
-            slow_print("\nRegeneration done.")
-
-if __name__ == "__main__":
-    cards = Deck().cards
-    count = 0
-    for card in cards:
-        slow_print(card, end=" ")
-        count += 1
-        if count % 13 == 0:
-            print()
+            print("\nRegeneration done.")
