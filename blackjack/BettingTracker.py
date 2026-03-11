64bfa1a15f032125544d6f9f89b9a0a9e35b0f95
Louie Jay Oga <kmshinichi@gmail.com>
2026-03-11 13:28:18 +0800

    Betting History
    Will track the betting history of the player for the machine to study and take advantage of if ever.

--- blackjack/BettingTracker.py
+++ blackjack/BettingTracker.py
@@ -1,1 +1,11 @@
+class BetTracker:
+    def __init__(self):
+        self.history = [] # list of {"bet": amount, "result": "win"/"loss"/"tie"}
+        self.streak = 0 # current consecutive win streak which will be used in machine learning later.
 
+    def update(self, bet_amount, result):
+        self.history.append({"bet": bet_amount, "result": result})
+        if result == "win":
+            self.streak += 1
+        else:
+            self.streak = 0
