bd874ec22a5d20c3ada8f2e9a9c9ad5bf22ea6b1
Louie Jay Oga <kmshinichi@gmail.com>
2026-03-14 13:59:01 +0800

    Added betting functionality and balance management

--- blackjack/Mechanics.py
+++ blackjack/Mechanics.py
@@ -7,15 +7,49 @@ class Player:
         "6": 6, "5": 5, "4": 4, "3": 3, "2": 2
     }
 
-    p_score, d_score = 0, 0
-
-    def __init__(self, name):
+    def __init__(self, name, balance = 1000):
         self.name = name
         self.hand = []
         self.score = 0
+        self.balance = balance  # The Default balance will always be 1000 for every user or 'object'.
+        self.bet = 0  # Player's bet
+        self.previous_bet = 0  # ML uses this to see how player's bet changed round to round.
+
+    def place_bet(self, amount):
+        if amount <= 0:
+            return False
+        if amount > self.balance:
+            return False
+
+        self.previous_bet = self.bet
+        self.bet = amount
+        self.balance -= self.bet
+        return True
+
+    def win_bet(self): # For v.s. Machine only
+        self.balance += self.bet * 2
+        self.bet = 0
+
+    def win_bet_pvp(self, opponent_bet): # Figure out how to do handle this in real time.
+        self.balance += self.bet + opponent_bet
+        self.bet = 0
+
+    def win_blackjack_dealer(self): # For v.s. Machine only
+        self.balance += int(self.bet * 2.5)
+        self.bet = 0
+
+    def lose_bet(self):
+        self.bet = 0
+
+    def tie(self):
+        self.balance += self.bet
+        self.bet = 0
+
+    def broke(self):
+        return self.balance <= 0
 
     def draw_card(self, deck):
-        self.hand.append(deck.draw())
+            self.hand.append(deck.draw())
 
     def calculation(self):
         total, aces = 0, 0
@@ -29,12 +63,11 @@ class Player:
             aces -= 1
         return total
 
-    def show_hand(self, hide_first = False):
+    def get_hand(self, hide_first=False):
+        """Returns the hand as a list. GUI will use this to render cards."""
         if hide_first:
-            print("?", *self.hand[1:])
-        else:
-            print(*self.hand)
+            return ["?"] + self.hand[1:]
+        return list(self.hand)
 
     def hand_reset(self):
         self.hand.clear()
- 
